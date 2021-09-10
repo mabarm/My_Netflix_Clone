c1bab6e139936d4fb4e0ff061c1a525f865c6277
Mamata Barman <mamata.android@gmail.com>
2021-09-10 23:56:06 +0530

    feat: basic structure

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Home from './pages/home/Home'
+export  const App=()=>{
+  return(
+        <div>
+          <h1>App</h1>
+          <Home />
+        </div> 
+ )
+}
+//export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import "./Home.scss";
+// import NavBar from ;
+// import Featured from ;
+//import List from ;
+
+const Home=()=>{
+    return(
+        <div className ="home">
+            <h1>In home </h1>
+        { /* <NavBar />
+         <Featured />
+        <List />
+        <List />
+        <List />
+        <List /> 
+        */ }
+        </div>
+    );
+}
+
+export default Home;
